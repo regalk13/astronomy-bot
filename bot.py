af4398a9796d9c988bb35f45cc42edcef3abf2d5
Juan Salazar <jsalazarcaviedes30@gmail.com>
2021-07-04 17:18:43 -0500

    Adding new commands

--- bot.py
+++ bot.py
@@ -3,6 +3,9 @@ import discord
 from discord.ext import commands
 import requests
 from googletrans import Translator
+import random
+
+from requests.api import request
 
 client = commands.Bot(command_prefix = '>')
 
@@ -16,7 +19,7 @@ async def ping(ctx):
     await ctx.send("Pong!")
 
 @client.command()
-async def image(ctx):
+async def daily(ctx):
     translator = Translator()
     response = requests.get("https://api.nasa.gov/planetary/apod?api_key=ol67wRzFoXgOlng82F2pHe8t6iezEbb9vxCbWJil")
     getters = response.json()
@@ -37,4 +40,72 @@ async def image(ctx):
 
     await ctx.send(embed=embed)
 
-client.run('ODYxMjk4MDQwNTE3Mjk2MTM4.YOHwTw.o1AAoMIDmcezW_Wdq8vxwdG7UYI')
+@client.command()
+async def rober(ctx, *, target):
+    response = requests.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=ol67wRzFoXgOlng82F2pHe8t6iezEbb9vxCbWJil")
+    getters = response.json()
+    random_number = random.randint(1, 1000)
+    image = getters['photos']
+    images = image[random_number]
+    image_src = images['img_src']
+    bot = images['rover']
+    bot_name = bot['name']
+    date = images['earth_date']
+
+    embed = discord.Embed(
+        title = "Imagen de marte:",
+        description = f"Imagen tomada por **{bot_name}** el _{date}_ :",
+        colour = discord.Colour.red()
+    )
+
+    embed.set_footer(text='Powered by Curiosity bot.', icon_url=ctx.author.avatar_url)
+    embed.set_image(url=image_src)
+
+    await ctx.send(embed=embed)
+
+
+@client.command()
+async def data(ctx,*,target):
+    translator = Translator()
+    response = requests.get(f"https://api.le-systeme-solaire.net/rest/bodies/{target}")
+    getters = response.json()
+    name = getters['englishName']
+    name_es = translator.translate(name, dest="es")
+    planet  = getters['isPlanet']
+    mass_data = getters['mass']
+    mass_value = mass_data['massValue']
+    vol_data = getters['vol']
+    vol_value = vol_data['volValue']
+    density = getters['density']
+    gravity = getters['gravity']
+
+
+    embed = discord.Embed(
+        title = f"Datos de {name_es.text}:",
+        colour = discord.Colour.red()
+    )
+
+    embed.add_field(name="Aqui encontraras datos exactos acerca de tu peticion:", value=f'> Nombre: {name}/{name_es.text}.\n> Planeta: {planet}.\n> Masa: {mass_value}.\n> Volumen: {vol_value}.\n> Densidad: {density}.\n> Gravedad: {gravity}.',inline=False)
+    embed.set_footer(text='Powered by Solaire API.')
+
+    await ctx.send(embed=embed)
+
+@client.command()
+async def iss(ctx):
+    response = requests.get("https://api.wheretheiss.at/v1/satellites/25544")
+    getters = response.json()
+    iss_latitude = getters['latitude']
+    iss_longitude = getters['longitude']
+    iss_altitude = getters['altitude']
+    iss_velocity = getters['velocity']
+
+    embed = discord.Embed(
+        title="Estación espacial",
+        colour = discord.Colour.green()
+    )
+
+    embed.add_field(name=f'**Datos en base al sitio de toma:**', value=f'> Latitud: {iss_latitude}Km.\n> Longitud: {iss_longitude}Km.\n> Altitud: {iss_altitude}Km.\n> Velocidad: {iss_velocity}K/h.',inline=False)
+
+    await ctx.send(embed=embed)
+
+client.run('ODYxMjk4MDQwNTE3Mjk2MTM4.YOHwTw.oFFFpTaCujN4uNJATvhZnlbj8ig')
